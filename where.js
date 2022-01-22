4c26907e9b90c9a79d93002ddb8462d70463fcb4
ariful4082 <ariful4082@gmail.com>
2022-01-22 20:47:36 +0600

    window aler

--- where.js
+++ where.js
@@ -1,3 +1,5 @@
 function myFunc() {
-  document.getElementById("demo").innerHTML = "Javascript calculation "+ ( 5 + 6);
+//   document.getElementById("demo").innerHTML = "Javascript calculation "+ ( 5 + 6);
+window.alert(5+6)
 }
+document.write(5+6)
